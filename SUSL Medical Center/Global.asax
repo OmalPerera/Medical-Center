4ee60caa4a3677406e5992dfbc6a6b0784f02d26
Omal Perera <omalperera8@gmail.com>
2016-08-01 18:34:45 +0530

    Initial Commit

---
+++ SUSL Medical Center/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SUSL_Medical_Center.MvcApplication" Language="C#" %>
---
+++ SUSL Medical Center/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SUSL_Medical_Center.Startup))]
+namespace SUSL_Medical_Center
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
